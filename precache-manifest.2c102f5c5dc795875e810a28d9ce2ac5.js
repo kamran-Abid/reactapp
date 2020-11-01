0e08852b6bcc3162c41f9c65779a07f745ef60ee
Kamran-Abid <zkabid911@gmail.com>
2020-11-01 07:57:27 -0800

    Updates

---
+++ precache-manifest.2c102f5c5dc795875e810a28d9ce2ac5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9adfc96dde3276a269e214acd066d6c2",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "e8396a1bd5066abe4578",
+    "url": "/reactapp/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "f3c60745e3ffacb09e42",
+    "url": "/reactapp/static/css/main.f7f9dafa.chunk.css"
+  },
+  {
+    "revision": "e8396a1bd5066abe4578",
+    "url": "/reactapp/static/js/2.863e3756.chunk.js"
+  },
+  {
+    "revision": "e60f6644a238030ec5fb7683004cdede",
+    "url": "/reactapp/static/js/2.863e3756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3c60745e3ffacb09e42",
+    "url": "/reactapp/static/js/main.b493398c.chunk.js"
+  },
+  {
+    "revision": "7325850d54f596a589d5",
+    "url": "/reactapp/static/js/runtime-main.f9e8ea19.js"
+  },
+  {
+    "revision": "45ca80f83ca8fa201106cdfee822db51",
+    "url": "/reactapp/static/media/i1.45ca80f8.jpg"
+  },
+  {
+    "revision": "a98a1fabf65d7de8cc5a6e70279e7c22",
+    "url": "/reactapp/static/media/i2.a98a1fab.jpg"
+  },
+  {
+    "revision": "65037455444e771f193dd0be49ccb54c",
+    "url": "/reactapp/static/media/i3.65037455.jpg"
+  },
+  {
+    "revision": "b8306f3d539e459ab973eed87db44132",
+    "url": "/reactapp/static/media/i4.b8306f3d.jpg"
+  },
+  {
+    "revision": "02ecb14da59339f5c9fc9ab9e15259ef",
+    "url": "/reactapp/static/media/ig1.02ecb14d.jpg"
+  },
+  {
+    "revision": "2c3b522d4b1b836a63a271bbaa4baee9",
+    "url": "/reactapp/static/media/ig2.2c3b522d.jpg"
+  },
+  {
+    "revision": "3421c39db87c20613b42de7b8a49b0b6",
+    "url": "/reactapp/static/media/ig3.3421c39d.jpg"
+  },
+  {
+    "revision": "7195526cdc03183bd21243ffdb88cbbc",
+    "url": "/reactapp/static/media/ig4.7195526c.jpg"
+  },
+  {
+    "revision": "e1ea53efdd6b65307ce4fc93afab2cd1",
+    "url": "/reactapp/static/media/img3.e1ea53ef.png"
+  },
+  {
+    "revision": "1b4b87829cceebaebd527b6e93e931e6",
+    "url": "/reactapp/static/media/img4.1b4b8782.png"
+  }
+]);
